e57b9aa4fadd034bb4140657069bc0f0ccb065df
Jonathan Gerber <jlgerber@gmail.com>
2021-10-24 21:54:50 -0700

    add comments for starting from scratch

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- Init.lua 
 --
+-- Note: I am using packer to manage nvim packages. To use on linux or osx, issue the following command:
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+-- you will also probably have to issue a :PackerInstall command from nvim 
+-- and you will have to follow the directions in lua/plugins.lua to install patched fonts
+--
 -- Initialize neovim using lua
 --
 -- global configuraiton
